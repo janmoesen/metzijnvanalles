67b9377ee6b06ba950ee08211536892a3b5231be
Jan Moesen <jan@tervelo.com>
2013-04-14 00:07:02 +0200

    Log to the JS console on JS 404

--- 404.php
+++ 404.php
@@ -2,4 +2,8 @@
 
 header('Content-Type: text/plain; charset=UTF-8');
 $urlInfo = parse_url($_SERVER['REQUEST_URI']);
-var_dump($urlInfo);
+
+if (isset($urlInfo['path']) && preg_match('/\.js$/', $urlInfo['path'])) {
+	header('Status: 200');
+	echo 'window.console && console.debug("Nope...");';
+}
