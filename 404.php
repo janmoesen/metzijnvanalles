1e22c5b5933cb4fe0a39f4230eb5ccdca78e0f4c
Jan Moesen <jan@tervelo.com>
2013-04-14 15:20:03 +0200

    404: set Content-Type header for JS

--- 404.php
+++ 404.php
@@ -5,5 +5,6 @@ $urlInfo = parse_url($_SERVER['REQUEST_URI']);
 
 if (isset($urlInfo['path']) && preg_match('/\.js$/', $urlInfo['path'])) {
 	header('Status: 200');
-	echo 'window.console && console.debug("Nope...");';
+	header('Content-Type: application/javascript; charset=UTF-8');
+	echo 'window.console && console.debug("Noppes, maar blijf vooral proberen…");';
 }
